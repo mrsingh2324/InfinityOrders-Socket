654996e20e97db55431b5eb07d12d541b3d2f185
Satyam Singh <87685197+mrsingh2324@users.noreply.github.com>
2023-12-25 12:46:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
 
 require("dotenv").config({
   path: "./.env",
 });
+
+const io = require("socket.io")(server, { cors: { origin: "*" } });
 
 app.use(cors());
 app.use(express.json());
@@ -50,6 +51,9 @@ io.on("connection", (socket) => {
     addUser(userId, socket.id);
     io.emit("getUsers", users);
   });
+  socket.on("error", (error) => {
+    console.error(`Socket error: ${error.message}`);
+  });'
 
   // send and get message
   const messages = {}; // Object to track messages sent to each user
